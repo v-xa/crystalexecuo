b5862ad932fbf16963ffc9820ffa8fc6c3ceda50
ブイ・エックス・エー <mliszewski0001@gmail.com>
2025-04-13 08:14:30 -0700

    Update useragent shit.lua

--- useragent shit.lua
+++ useragent shit.lua
@@ -2,7 +2,7 @@ local response = http_request({
     Url = "https://httpbin.org/headers",
     Method = "GET",
     Headers = {
-        ["User-Agent"] = "crystal by mati at discord.gg/getcrystal"
+        ["User-Agent"] = "CrystalAPI"
     }
 })
 
